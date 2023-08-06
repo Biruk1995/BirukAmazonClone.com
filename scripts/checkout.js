9ce4bd258653f60b8396f5fbe0918879e94c3c8d
birukBejga <birukbejga8@gmail.com>
2023-08-06 09:15:48 +0300

    delete cart item HTML from the page on clicking delet

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -12,7 +12,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${machingproduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -99,6 +99,10 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       removeCartItem(productId);
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      )
+        container.remove();
     });
   });
 
